d1647892436df42c96ec4916015d00eaf0766c26
Buddhima Hewavitharana <buddhimahewa@gmail.com>
2022-08-18 19:32:23 +0530

    inital commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import NavBar from './components/NavBar'
+import Grade from './components/Grade';
+import Subject from './components/Subject';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar/>
+      <Grade/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/NavBar.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import '../common.css'
+
+export default function NavBar() {
+    return (
+        <nav className="navbar">
+            <ul>
+                <li><img src="logo.png" alt="logo"></img></li> 
+                <li>STUDENTS</li> 
+                <li>TEACHERS</li>
+                <li>HOME</li>
+                <li><button id="changepsw-btn">CHANGE<br></br>PASSWORD</button></li>
+                <li><button id="logout-btn">LOGOUT</button></li>
+            </ul>
+        </nav>
+    )  
+}       
